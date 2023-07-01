b1d1a6ac7c935147121ada7dcfe07c10eb4e343d
Maksim Zhorzh <maxim.zhorzh@yandex.ru>
2023-07-01 19:59:16 +0300

    Добавил параметризированные тесты для класса Булки

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,11 +1,43 @@
 import praktikum.Bun;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+@RunWith(Parameterized.class)
 public class BunTest {
-    private final String name = "black bun";
-    private final Float price = 100F;
-    private final Bun bun = new Bun(name, price);
+    private final String name;
+    private final Float price;
+    private Bun bun;
+
+    public BunTest(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getTestData() {
+        return new Object[][] {
+                {"white bun",210},
+                {"black bun",100},
+                {"brown bun",350},
+                {null,0},
+                {"",1},
+                {"булкабулкабулкабулкабулкабулкабулкабулкабулкабулкабулкабулкабулкабулкабулкабулкабулкабулкабулкабулкабулкабулка",Float.MAX_VALUE},
+                {"W",Float.MIN_VALUE},
+                {"@![&o*as",-100},
+                {"white bun",0.1F},
+                {"brown bun",-0.001F},
+                {"121212",100}
+        };
+    }
+
+    @Before
+    public void setUp(){
+        bun = new Bun(name, price);
+    }
+
     @Test
     public void checkGetNameReturnName() {
         Assert.assertEquals(name, bun.getName());
